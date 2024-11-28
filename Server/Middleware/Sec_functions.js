6efe4f54771975c50ec0ee30f4cc7c84e7e74a7f
AbdelrahmanElkhouly <boody.elkhouly@gmail.com>
2024-11-28 13:01:14 +0200

    Sec_functions.js
    i added comments incase anyone wants to understand, these comments are to be removed before submission
    
    there is a part in getuser(req) is incomplete

--- Server/Middleware/Sec_functions.js
+++ Server/Middleware/Sec_functions.js
@@ -1,11 +1,47 @@
 const DB= require('../DB/Index.js')
 
 function get_session_tok(req){
-
+  //check first if the user has a cookie
+  if(!req.headers.cookie){
+    return null
+  }
+  //refers to the cookie, splits it into an array, map allows it to pass by each element of the array, trim() removes unnecessary spaces like " hello world " >> "hello world"
+  //after trimming, we pass by each element of the array using filter() to isolate the specific part of the cookie that has the session token, we check its presence using includes()
+  //after filtering the array and getting the element that has the session token, we use join('') to compress the element like "hello world" >> "helloworld"
+  const cookies = req.headers.cookie.split(';')
+    .map(function (cookie) { return cookie.trim() })
+    .filter(function (cookie) { return cookie.includes('session_token') })
+    .join('');
+//in order to extract the session token from the element that we got, we use slice(), slice() returns a specified part of an array without modifying the original array
+  const sessionToken = cookies.slice('session_token='.length);
+//check if there is a session token in the cookie  
+  if (!sessionToken) {
+    return null;
+  }
+  return sessionToken;
 
 }
 
-function get_user(req){
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
+function get_user(req){
+//check if the user has a session token otherwise, redirect back to '/'
+    const sessionToken = getSessionToken(req);
+  if (!sessionToken) {
+    console.log("no session token is found")
+    return res.status(301).redirect('/');
+  }
+
+  //query the db to search for the user that has this session token
+  //note: this part is incomplete as i need to know the names of the schemas/tables
+  const user = await db.select('*')
+    .from({ s: 'backendTutorial.Session' })
+    .where('token', sessionToken)
+    .innerJoin('backendTutorial.User as u', 's.userId', 'u.id')
+    .first(); 
+
+  
+  console.log('user =>', user)
+  return user;  
 
 }
