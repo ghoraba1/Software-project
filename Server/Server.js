929d4413d9d8bee6ab9583a03a901152447c1bb2
motawe3theking <modymocho@gmail.com>
2024-12-07 01:06:20 +0200

    Update Server.js

--- Server/Server.js
+++ Server/Server.js
@@ -1,18 +1,20 @@
-
+const express = require("express");
 require('dotenv').config();
+
 const {PublicAPIs} = require('./APIs/Public_APIs.js')
 const {EquipmentAPIs} = require('./APIs/Equipment.js')
-const {CartAPIs} = require('./APIs/Cart.js')
+const {CartAPIs} = require('./APIs/RatingCartOrder.js')
 const {UserAPIs} = require('./APIs/User.js')
 const{Middleware} =require('./Middleware/Admin.js')
 
-const express = require("express") ;    
-const  app=express() ;
+const app = express();
 app.use(express.json())
-const PORT = process.env.PORT;
 
+const PORT = process.env.PORT;
 app.listen(PORT,()=>{
     console.log(`Server is listening to port ${PORT}`) 
 }) ;
 
 UserAPIs(app);
+PublicAPIs(app);
+app.use(Middleware);
