1aaec4ce35d0c91c1608c16635ac38fbf344b50c
motawe3theking <modymocho@gmail.com>
2024-12-08 00:13:44 +0200

    hirarchy

--- Server/Server.js
+++ Server/Server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 require('dotenv').config();
 
-const {PublicAPIs} = require('./APIs/Public_APIs.js')
-const {EquipmentAPIs} = require('./APIs/Equipment.js')
-const {CartAPIs} = require('./APIs/RatingCartOrder.js')
-const {UserAPIs} = require('./APIs/User.js')
-const{Middleware} =require('./Middleware/Admin.js')
+const {HandlePublicAPIs} = require('./APIs/Public/Public_APIs.js')
+// const {EquipmentAPIs} = require('./APIs/Equipment.js')
+// const {CartAPIs} = require('./APIs/RatingCartOrder.js')
+// const {UserAPIs} = require('./APIs/User.js')
+const{Middleware} =require('./Middleware/Admin.js');
+const {HandlePrivateAPIs} = require("./APIs/Private/PrivateAPIs.js");
 
 const app = express();
 app.use(express.json())
@@ -13,8 +14,8 @@ app.use(express.json())
 const PORT = process.env.PORT;
 app.listen(PORT,()=>{
     console.log(`Server is listening to port ${PORT}`) 
-}) ;
+});
 
-UserAPIs(app);
-PublicAPIs(app);
+HandlePublicAPIs(app);
 app.use(Middleware);
+HandlePrivateAPIs(app);
