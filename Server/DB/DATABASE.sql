15a6dc8772083ee49b3e0789b2f7820f4a56e45e
AbdelrahmanElkhouly <boody.elkhouly@gmail.com>
2024-12-11 21:23:04 +0200

    DATABASE.sql
    There must be a default value for the "role" of users table
    the user is standard by default

--- Server/DB/DATABASE.sql
+++ Server/DB/DATABASE.sql
@@ -32,7 +32,7 @@ user_ID serial primary key ,
 username text NOT NULL ,
 email text not null,
 password text not null,
-role text not null,
+role text DEFAULT 'standard_user' not null,
 created_at Date not null
 );
 
