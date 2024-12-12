caccb84208eda66a6ed3ae285528ed4b3842ed5c
GHORABA <Abdelrahman.ghoraba1@gmail.com>
2024-12-12 19:08:55 +0200

    BigThingshappening_2

--- Server/APIs/Public/Public_APIs.js
+++ Server/APIs/Public/Public_APIs.js
@@ -2,13 +2,14 @@ const DB = require('../../DB/Index.js')
 const bcrypt =require('bcrypt') ; 
 const saltRounds =10 ;
 //User APIs will access the database so we need to include th file 
-
+const { v4 } = require('uuid');
+//we need this to generate UUID (A UUID (Universally Unique Identifier) is a 128-bit number used to uniquely identify objects or entities in computer systems. Here are some key points about UUIDs)
 function HandlePublicAPIs(app){
 
 
-    app.post('/api/v1/users/new',async(req,res)=>{
-
-      const userExists = await DB.select('*').from('public.users').where('email', req.body.email);
+    app.post('/api/v1/users/new',async(req,res)=>{ //register new user 
+     //Checking if user exists using email.
+      const userExists = await DB.select('*').from('public.users').where('email', req.body.email); 
       console.log("UE",userExists)
       if (userExists.length > 0) {
         return res.status(400).send('user exists');
@@ -35,10 +36,11 @@ function HandlePublicAPIs(app){
     app.put('/api/V1/users/:id',async(req,res)=>{
       try{
          const{username,email,password,role}=req.body ;
+         const hashedPassword = await bcrypt.hash(password, saltRounds); 
          const query=`UPDATE users
          set username='${username}',
          email='${email}',
-         password=crypt('${password}',gen_salt(bf)),
+         password=${hashedPassword},
          role='${role}'
          WHERE id=${req.params.id}`
          
@@ -86,6 +88,62 @@ function HandlePublicAPIs(app){
           return res.status(400).send("Unable to view ratings.");
         }
       });
+
+
+      app.post('/api/v1/user/login', async function(req, res) {
+        // get users credentials from the JSON body
+        const { email, password } = req.body
+        if (!email) {
+          // If the email is not present, return an HTTP unauthorized code
+          return res.status(400).send('email is required');
+        }
+        if (!password) {
+          // If the password is not present, return an HTTP unauthorized code
+          return res.status(400).send('Password is required');
+        }
+  
+        // validate the provided password against the password in the database
+        // if invalid, send an unauthorized code
+        let user = await DB.select('*').from('public.users').where('email', email);
+        console.log("user : : ",user)
+        if (user.length == 0) {
+          return res.status(400).send('user does not exist');
+        }
+        user = user[0];
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        if (user.password !== hashedPassword) {
+          return res.status(400).send('Password does not match');
+        }
+  
+        // set the expiry time as 30 minutes after the current time
+        const token = v4();
+        const currentDateTime = new Date();
+        const expiresAt = new Date(+currentDateTime + 18000000); // expire in 3 minutes
+  
+        // create a session containing information about the user and expiry time
+        const session = {
+          user_id: user.id,
+          token,
+          expiresAt,
+        };
+        try {
+          await DB('public.Session').insert(session);
+          // In the response, set a cookie on the client with the name "session_cookie"
+          // and the value as the UUID we generated. We also set the expiration time.
+          return res.cookie("session_token", token, { expires: expiresAt }).status(200).send('login successful');
+        } catch (e) {
+          console.log(e.message);
+          return res.status(400).send('Could not register user');
+        }
+      });
+  
+
+
+
+
+
+      
 }
         // We will export the UserAPIs to be able to use it in the server_mainfile 
 module.exports={HandlePublicAPIs} ;
