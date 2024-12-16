3d913321ab8ce292ab07ff170da11b581ca736a8
motawe3theking <modymocho@gmail.com>
2024-12-16 16:08:47 +0200

    jqyery

---
+++ Client/public/Src/login.js
@@ -0,0 +1,30 @@
+$ (function(){
+    $("#loginBtn").on("click", function() {
+      const email = $('#email').val();
+      const password = $('#password').val();
+
+      const data = {
+        email,
+        password,
+      };
+      console.log(data);
+
+      $.ajax({
+        type: "POST",
+        url: '/api/v1/user/login',
+        data  ,
+        success: function(serverResponse) {
+          if(serverResponse) {
+            alert("login successfully");
+            console.log("got to dahsboard")
+            location.href = '/equipmentmangement';
+          }
+        },
+        error: function(errorResponse) {
+          if(errorResponse) {
+            alert(`User login error: ${errorResponse.responseText}`);
+          }            
+        }
+      });
+    });
+  });
