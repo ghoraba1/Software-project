4bc6f3910c417232d09725f03e833b17ac57c7b8
GHORABA <Abdelrahman.ghoraba1@gmail.com>
2024-12-18 14:35:46 +0200

    lOGIN

--- Server/APIs/Private/view.js
+++ Server/APIs/Private/view.js
@@ -9,8 +9,24 @@ function HandlePrivateFrontEndView(app){
     try {
       const user = await get_user(req, res);
       if (user.role === 'admin') {
-        return res.render('EquipmentMangmentAdmin');
-      } else {
+        return res.render('HomeAdmin');
+      } 
+      else {
+        return res.status(403).send('Access denied');
+      }
+    } catch (error) {
+      console.error('Error fetching user:', error);
+      return res.status(500).send('Internal Server Error');
+    }
+  });+
+
+  app.get('/HomeUser', async (req, res) => {
+    try {
+      const user = await get_user(req, res);
+      if (user.role === 'standard_user') {
+        return res.render('HomeUser');
+      } 
+      else {
         return res.status(403).send('Access denied');
       }
     } catch (error) {
@@ -18,6 +34,7 @@ function HandlePrivateFrontEndView(app){
       return res.status(500).send('Internal Server Error');
     }
   });
+
 
 
 
